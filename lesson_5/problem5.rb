dacfdab52271f1e637af7c64c463bdf45015063b
Julia-Park <julia.ay.park@gmail.com>
2020-07-16 21:45:26 -0400

    Add 5 practice problems from Lesson 5

---
+++ lesson_5/problem5.rb
@@ -0,0 +1,17 @@
+# find total age of male members of family
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_age = 0
+
+munsters.each do |name, values|
+  total_age += values['age'] if values['gender'] == 'male' 
+end
+
+p total_age
