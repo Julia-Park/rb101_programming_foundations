4130ce31d39a1e4e3227f32679d1c212192b0bf3
Julia-Park <julia.ay.park@gmail.com>
2020-07-30 14:35:31 -0400

    Modified twenty_one.rb and twenty_one_messages.yml to complete gameplay elements

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -2,8 +2,11 @@ require 'yaml'
 require 'pry'
 
 MESSAGES = YAML.load_file('twenty_one_messages.yml')
+SUIT_CARDS = 'KQJA23456789'
 DEALER = 'Dealer'
 PLAYER = 'Player'
+DEALER_THRESHOLD = 17
+BUST_VALUE = 21
 
 def prompt(msg)
   puts ">> #{msg}"
@@ -20,8 +23,12 @@ def join_and(array, delimiter = ', ', last_delimiter = 'and')
 end
 
 def initialize_deck
-  deck = 'KQJA23456789'.chars * 4
+  deck = SUIT_CARDS.chars * 4
   deck.shuffle
+end
+
+def initialize_hands
+  hands = { PLAYER => [], DEALER => [] }
 end
 
 def deal_cards!(deck, hand, num_cards = 2)
@@ -46,10 +53,23 @@ def card_value(card)
   end
 end
 
-def list_cards(hand, owner, hide_last_card = false)
-  cards = hand.dup
-  cards[-1] = 'unknown card' if hide_last_card 
-  prompt format(MESSAGES['show_hand'], owner, join_and(cards))
+def list_cards(hands, owner, hide_last_card = false)
+  cards = hands[owner].map { |card| card_name(card) }
+  if hide_last_card
+    prompt format(MESSAGES['show_hand_hidden'], owner, cards[0])
+  else
+    prompt format(MESSAGES['show_hand'], owner, join_and(cards), card_sum(hands[owner]))
+  end
+end
+
+def list_sums(hands)
+  message = []
+
+  hands.each do |owner, hand|
+    message << format(MESSAGES['totals'], owner, card_sum(hand))
+  end
+
+  prompt join_and(message)
 end
 
 def choose_move
@@ -57,72 +77,132 @@ def choose_move
   loop do
     prompt MESSAGES['choose_move']
     answer = gets.chomp.downcase
-    break if ['hit', 'stay', 'h', 's'].include?(answer)
+    break if %w(hit stay h s).include?(answer)
     prompt MESSAGES['invalid_choice']
   end
   answer
 end
 
 def card_sum(hand)
-  # Ace = 11 unless sum of other cards is > 10
-  # START HEREEE
-  if hand.include?('A')
-    # find first Ace
-    hand.index('A')
-    # remove an Ace
+  # First Ace = 11 unless sum of other cards is > 10
+  sum = 0
+  first_ace_position = hand.index('A') # nil if no Ace
 
-    hand.each
+  hand.each.with_index do |card, index|
+    sum += card_value(card) if first_ace_position != index
+  end
 
-    sum = hand.reduce do |sum, card| 
-      sum += card_value(card)
-    end
-    case sum
-    when 1..10
-      sum+=11
-    else
-      sum+=1
-    end
+  if !!first_ace_position # if there is an Ace
+    sum > 10 ? sum += 1 : sum += 11
+  end
 
-  else
-    hand.reduce do |sum, card| 
-      sum += card_value(card)
-    end
+  sum
+end
+
+def any_bust?(hands) 
+  hands.any? { |_, hand| bust?(hand) }
+end
+
+def bust?(hand)
+  card_sum(hand) > BUST_VALUE
+end
+
+def who_busted(hands)
+  busted = hands.select { |owner, hand| bust?(hand) }.keys
+  prompt format(MESSAGES['bust'], busted[0], other_player(busted[0]))
+end
+
+def who_won(hands)
+  max_sum = hands.map { |owner, hand| card_sum(hand) }.max
+
+  winner = hands.select { |owner, hand| card_sum(hand) == max_sum }.keys
+
+  if winner.size == 1
+    prompt format(MESSAGES['win'], winner[0])
+  else 
+    prompt MESSAGES['tie']
   end
 end
 
-def bust?(hand) 
-  card_sum(hand) > 21
+def other_player(name)
+  case name
+  when PLAYER then DEALER
+  when DEALER then PLAYER
+  end
 end
 
-def win?(hand)
-  card_sum(hand) == 21
-end
-# Game Logic Start
-game_deck = initialize_deck
-player_hand = []
-dealer_hand = []
-
-deal_cards!(game_deck, player_hand)
-deal_cards!(game_deck, dealer_hand)
-
-system 'clear'
-
-list_cards(player_hand, PLAYER)
-list_cards(dealer_hand, DEALER, true)
-
-loop do # Player turn
-  answer = choose_move
-  break if answer == 'stay'
-
-  deal_cards!(game_deck, player_hand, 1)
-  list_cards(player_hand, PLAYER)
+def play_again?
+  answer = ''
+  loop do
+    prompt MESSAGES['play_again']
+    answer = gets.chomp.downcase
+    break if %w(yes no y n).include?(answer)
+    prompt MESSAGES['invalid_choice']
+  end
+  %w(y yes).include?(answer)
 end
 
-# 3. Player turn - choose between hit and stay
-#   - Repeat until bust or stay
-#     - If bust, round over.  Dealer wins.
-# 4. Dealer turn - choose between hit (if sum < 17) and stay
-#   - Repeat until bust or stay
-#   - If bust, round over.  Player wins.
-# 5. Determine winner
-#   - Dealer/Player with highest sum wins.
+# Game Start
+loop do
+  # Set up
+  game_deck = initialize_deck
+  game_hands = initialize_hands
+
+  deal_cards!(game_deck, game_hands[PLAYER])
+  deal_cards!(game_deck, game_hands[DEALER])
+
+  current_player = PLAYER
+
+  # Turns
+  2.times do
+    case current_player
+    when PLAYER # Player turn
+      loop do
+        system 'clear'
+
+        list_cards(game_hands, DEALER, true)
+        list_cards(game_hands, PLAYER)
+
+        answer = choose_move
+        break if %w(stay s).include?(answer)
+
+        deal_cards!(game_deck, game_hands[PLAYER], 1)
+
+        list_cards(game_hands, PLAYER)
+        break if any_bust?(game_hands)
+      end
+
+      current_player = other_player(current_player)
+
+    when DEALER # Dealer turn
+      loop do
+        list_cards(game_hands, DEALER)
+        break if any_bust?(game_hands)                          # bust
+
+        if card_sum(game_hands[DEALER]) < DEALER_THRESHOLD      # hit
+          deal_cards!(game_deck, game_hands[DEALER], 1)
+          prompt format(MESSAGES['hit'], DEALER)
+        else                                                    # stay
+          prompt format(MESSAGES['stay'], DEALER)
+          break
+        end
+      end
+    end
+
+    puts ''
+    break if any_bust?(game_hands)
+  end
+
+  # Determine winner
+  if any_bust?(game_hands)
+    who_busted(game_hands)
+  else
+    list_sums(game_hands)
+    who_won(game_hands)
+  end
+
+  break unless play_again?
+end
+
+puts ''
+prompt MESSAGES['thanks']
